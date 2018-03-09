b840ce7d20b73513684a99f25248564602764da3
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-03-09 16:03:53 +0900

    Add more elaborate example

--- architecture.py
+++ architecture.py
@@ -1,10 +1,11 @@
 from noh import Architecture
 
+
 class SeedWBA(Architecture):
     def __init__(self):
-        super(SeedWBA, self).__init__(['sa', 'amg', 'bg', 'hip', 'pfc'], [
+        super(SeedWBA, self).__init__(
             (('hip', 'sa'), 'bg'),
             (('amg', 'sa'), 'hip'),
             (('hip', 'bg'), 'pfc'),
             ('hip', 'amg'),
-        ])
+        )
